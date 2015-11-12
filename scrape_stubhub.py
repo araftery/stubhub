a45feaf2f14199e838cce8d9afa30f3499c12be6
Andrew Raftery <andrewraftery@gmail.com>
2015-11-12 15:37:26 -0500

    deal with exceptions

--- scrape_stubhub.py
+++ scrape_stubhub.py
@@ -3,8 +3,6 @@ import gzip
 import json
 import requests
 
-data = []
-
 # events per request
 rows = 5000
 listing_endpoint = 'http://www.stubhub.com/listingCatalog/select'
@@ -14,20 +12,33 @@ payload = {
     'q': 'stubhubDocumentType:event',
 }
 
-last_found = 1
 
-i = 0
-while last_found > 0:
-    payload['start'] = i * rows
-    r = requests.get(listing_endpoint, params=payload)
-    r.raise_for_status()
-    response = r.json()
-    events = response['response']['docs']
-    last_found = len(events)
-    data += events
-    i += 1
+if __name__ == '__main__':
+    times = 0
+    while True and times < 10:
+        data = []
+        try:
+            last_found = 1
+            i = 0
+            while last_found > 0:
+                payload['start'] = i * rows
+                r = requests.get(listing_endpoint, params=payload)
+                r.raise_for_status()
+                response = r.json()
+                events = response['response']['docs']
+                last_found = len(events)
+                data += events
+                i += 1
 
-
-now = datetime.datetime.now()
-with gzip.open('snapshot_{}_{}_{}_{}_{}.json'.format(now.month, now.day, now.year, now.hour, now.minute), 'wt') as outfile:
-    json.dump(data, outfile)
+            now = datetime.datetime.now()
+            with gzip.open('snapshot_{}_{}_{}_{}_{}.json'.format(now.month, now.day, now.year, now.hour, now.minute), 'wt') as outfile:
+                json.dump(data, outfile)
+            break
+        except:
+            times += 1
+            continue
+    else:
+        # failed
+        now = datetime.datetime.now()
+        with open('snapshot_{}_{}_{}_{}_{}_FAILED.json'.format(now.month, now.day, now.year, now.hour, now.minute), 'wt') as outfile:
+            outfile.write('FAIL')
